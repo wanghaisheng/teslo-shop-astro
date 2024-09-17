5c0a7201d363d6f75308b26fdebc088fb022f7ca
Freddy Alvarado Rodríguez <falvarador@Freddys-MacBook-Pro.local>
2024-09-16 22:55:43 -0600

    feat: Add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
+import db from "@astrojs/db";
+
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), db()],
+  output: "server",
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
---
+++ db/config.ts
@@ -0,0 +1,29 @@
+import { column, defineDb, defineTable } from 'astro:db';
+
+const User = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    username: column.text({uniqueKey: true, optional: false }),
+    password: column.text({optional: true }),
+  }
+});
+
+const Session = defineTable({
+  columns: {
+		id: column.text({
+			primaryKey: true
+		}),
+		expiresAt: column.date(),
+		userId: column.text({
+			references: () => User.columns.id
+		})
+	}
+});
+
+// https://astro.build/db/config
+export default defineDb({
+	tables: {
+		User,
+		Session
+	}
+});
